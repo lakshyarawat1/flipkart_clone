4a45639342c236f8398b8dba00bccbccecb797f9
lakshyarawat1 <lakshya.paramount@gmail.com>
2022-08-28 21:39:00 +0530

    updated search bar

--- client/src/components/header/search.jsx
+++ client/src/components/header/search.jsx
@@ -1,21 +1,33 @@
-import { InputBase , Box, styled } from '@mui/material';
+import { InputBase, Box, styled } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 
 const SearchContainer = styled(Box)`
     background : #fff;
-    width : 38%;
+    width : 34%;
     border-radius : 2px;
-    margin-left : 10px
+    margin-left : 10px;
+    display : flex;
 `;
 
+const SearchIconWrapper = styled(Box)`
+    color : blue;
+`
+
 const InputSearchBase = styled(InputBase)`
-    
+    padding-left : 20px;
+    width : 100%;
 `
 
 const Search = () => {
     return (
         <SearchContainer>
-            <InputSearchBase />
+            <InputSearchBase
+            placeholder ='Search for products, brands and more'
+            />
+            <SearchIconWrapper>
+                <SearchIcon />
+            </SearchIconWrapper>
         </SearchContainer>
     )
 }

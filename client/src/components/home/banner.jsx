03e8a3f102757ceb36ab99f9c07db1bb9cb1e5cb
lakshyarawat1 <lakshya.paramount@gmail.com>
2022-08-29 20:12:43 +0530

    created server

--- client/src/components/home/banner.jsx
+++ client/src/components/home/banner.jsx
@@ -25,7 +25,19 @@ const responsive = {
 
 const Banner = () => {
     return (
-        <Carousel responsive={responsive}>
+        <Carousel
+            responsive={responsive}
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+            containerClass="carousel-container"
+            swipeable={false}
+            draggable={false}
+            infinite={true}
+            autoPlay={true}
+            autoPlaySpeed={4000}
+            keyBoardControl={true}
+            slidesToSlide={1}
+        >
             {
                 bannerData.map(data => (
                     <div>
--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,14 +1,20 @@
-import { Fragment } from 'react';
+import { Box, styled } from '@mui/material'
 
 import NavBar from './Navbar'
 import Banner from './banner'
 
+const Container = styled(Box)`
+    padding : 20px 10px 10px 10px;
+    background : #f2f2f2;
+`
 
 const Home = () => {
     return (
         <>
             <NavBar />
-            <Banner />
+            <Container>
+                <Banner />
+            </Container>
         </>
     )
 }

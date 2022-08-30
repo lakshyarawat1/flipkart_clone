fdcd4fb81f0523d2315cff8d8db554816b3bb8eb
lakshyarawat1 <lakshya.paramount@gmail.com>
2022-08-30 14:33:29 +0530

    imported unique data

--- server/default.js
+++ server/default.js
@@ -1,9 +1,9 @@
 import { products } from './constants/data.js'
 import product from './models/productSchema.js'
 
-const defaultData = () => {
+const defaultData = async () => {
     try {
-        product.insertMany(products)
+        await product.insertMany(products)
         console.log('Data imported successfully');
     } catch (error) {
         console.log('Error while inserting default data', error.message);

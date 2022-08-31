69fbb68e7b36ce62e40234657f97c1a940518227
lakshyarawat1 <lakshya.paramount@gmail.com>
2022-08-31 15:36:42 +0530

    deleted unfunctionalities

--- server/routes/route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import userSignUp from '../controller/userController.js';
-
-const router = express.Router();
-
-router.post('/signup', userSignUp)
-
-export default router

c8aa057afcae4e6c648889c27ee8654ba7f3c4ba
lakshyarawat1 <lakshya.paramount@gmail.com>
2022-08-31 18:14:35 +0530

    recreated navbar

--- client/src/components/header/customButtons.jsx
+++ client/src/components/header/customButtons.jsx
@@ -1,64 +1,46 @@
-import { Box, Button, Typography ,styled } from '@mui/material'
-import ShoppingCart from '@mui/icons-material/ShoppingCart';
-import LoginDialog from '../login/loginDialog'
-import { useState } from 'react'
+import { Button, Box, Typography , styled } from '@mui/material'
+import { ShoppingCart } from '@mui/icons-material'
+
 
 const Wrapper = styled(Box)`
-    display:flex;
-    margin : 0 3% auto;
-    & > button, & > p, & > div {
-        margin-right : 40px;
-        font-size : 15px;
-        align-items : center;
-        font-weight : 600;
-    }
+    display : flex;
+`
+
+const LoginButton = styled(Button)`
+    background : #fff;
+    color : #2874f0;
+    height : 30px;
+    width : 125px;
+    text-transform : none;
+    font-weight : 600;
+    border-radius : 0;
+    box-shadow : 0;
 `
 
 const Container = styled(Box)`
     display : flex;
-    margin-bottom : 12px;
 `
-
-const LoginButton = styled(Button)`
-    color : #2874f0;
-    background : #fff;
-    text-transform : none;
-    padding : 5px 40px;
-    border-radius : 2px;
-    box-shadow : none;
-    font-weight : 600;
-    margin-bottom : 15px;
-    height : 32px;
-    margin-top : 7px;
-`
-
 
 const CustomButtons = () => {
-
-    const [ open, setOpen ] = useState(false);
-    const openDialog = () => {
-        setOpen(true);
-    }
     return (
         <Wrapper>
-            <LoginButton varient="contained" onClick={() => openDialog()}>
+            <LoginButton variant="contained" style={{ marginLeft : 25 }}>
                 Login
             </LoginButton>
-
-            <Typography style={{ marginTop : 12, width : 150 }}>
+            <Typography style={{ marginLeft : 40 }}>
                 Become a Seller
             </Typography>
-            <Typography style={{ marginTop: 12, marginRight : 54 }}>More</Typography>
-
+            <Typography style={{ marginLeft : 40 }}>
+                More
+            </Typography>
             <Container>
-                <ShoppingCart />
-                <Typography style={{ fontWeight : 600 }}>
+                <ShoppingCart style={{ marginLeft : 50 }} />
+                <Typography>
                     Cart
                 </Typography>
             </Container>
-            <LoginDialog open={open} setOpen={setOpen} />
         </Wrapper>
     )
 }
 
-export default CustomButtons;
+export default CustomButtons

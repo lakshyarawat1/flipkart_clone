d8101c139a11b0d1d9d9468be1fcac25ab24e6a9
lakshyarawat1 <lakshya.paramount@gmail.com>
2022-09-06 15:24:05 +0530

    created product route

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -10,3 +10,14 @@ export const getProducts = async (req, res) => {
     }
 
 }
+
+export const getProductById = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const product =await Product.findOne({ 'id' : id })     
+
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({ message : error.message })
+    }
+}
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { userSignUp, userLogin } from '../controller/userController.js'
-import { getProducts } from '../controller/productController.js'
+import { getProducts,  getProductById } from '../controller/productController.js'
 
 const router = express.Router()
 
@@ -9,5 +9,7 @@ router.post('/signup', userSignUp)
 router.post('/login', userLogin)
 
 router.get('/products', getProducts)
+
+router.get('/product/:id', getProductById)
 
 export default router

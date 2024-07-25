97d876e660679442c71c5e8eb0f474f15bb49b28
lakshyarawat1 <lakshya.paramount@gmail.com>
2024-07-25 17:30:08 +0530

    [ # 4 Update - Build Update ]

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
     method: ["GET", "POST"],
   })
 );
